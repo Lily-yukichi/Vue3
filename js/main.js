408b4378e9d7e05e70c5ef187a2d31a96855ce16
RUI <>
2022-11-21 22:57:17 +0900

    ★console.logはブラウザの検証画面で確認。 実行したい処理内容によって算出プロパティにするかmethodsにするか使い分けることが出来る。

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const app = Vue.createApp({
     data: () => ({
-       税抜価格: 100
+       
     }),
     computed: {
-        税込価格: {
-            get: function() {
-                return this.税抜価格 * 1.1
-            },
-            set: function(value) {
-                this.税抜価格 = value / 1.1
-            }
+        computedNumber: function(){
+            console.log('computed!')
+            return Math.random()
+        }
+    },
+    methods: {
+        methodsNumber: function() {
+            console.log('methods!')
+            return Math.random()
         }
     }
 })
